0d9d38776c47b9476f0cb46a9a2e07b1b41ce05b
Babu-b99 <prasathbabu399@gmail.com>
2021-12-07 13:33:56 +0530

    Task

---
+++ src/com/aaludra/Constructor.java
@@ -0,0 +1,60 @@
+package com.aaludra;
+import java.util.Scanner;
+
+public class Constructor {
+	private static int a;
+	private static int b;
+	private static int c;
+	private static int m,n;
+	public void Constructor(int a,int b,int c) {
+		this.a=a;
+		this.b=b;
+		this.c=c;
+	
+	}
+	public void array() {
+		
+		int[][] a=new int[100][100];
+		int[][] b=new int[100][100];
+		int[][] c=new int[100][100];
+		int m,n;
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the values of Rows and Columns");
+		m=in.nextInt();
+		n=in.nextInt();
+		System.out.println("Enter the value of A Matrix:");
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.println("a["+i+"]["+j+"]");
+				a[i][j]=in.nextInt();
+			}
+		}
+		System.out.println("Enter the value of B Matrix:");
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.println("b["+i+"]["+j+"]");
+				b[i][j]=in.nextInt();
+			}
+		}
+		System.out.println("Result");
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				c[i][j]=a[i][j]*b[i][j];
+				System.out.print("\t"+c[i][j]);
+			}
+			System.out.println("");
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Constructor obj=new Constructor();
+		obj.array();
+		
+		
+	}
+
+}
---
+++ src/com/aaludra/Task.java
@@ -0,0 +1,77 @@
+package com.aaludra;
+import java.io.*;
+import java.util.Arrays;
+public class Task {
+	private static int[] a=new int[]{50,40,60,70,80};
+	public void getFirstValue() {
+		
+			System.out.println("FirstElement:"+a[0]);	
+	}
+	public void getLastValue() {
+			System.out.println("Last Element"+a[4]);	
+	}
+	public void getAverage() {
+		
+		int sum=0;
+		for(int i=0;i<a.length;i++) {
+			
+			sum=sum+a[i];	
+		}
+		int avg=sum/a.length;
+		System.out.println("Average: "+avg);
+		}
+	public void Index() {
+		
+		int index=3;
+		int value=90;
+		System.out.println("Before:"+Arrays.toString(a));
+		for(int i=a.length-1;i>=index;i--) {
+			a[i]=a[i-1];
+		}
+		a[index]=value;
+		
+		System.out.println("After:"+Arrays.toString(a));
+	}
+	public void count() {
+		 int[] b = new int [] {1,2,1,3,5,1,1,6};  
+	       
+	        int[] c = new int [b.length];  
+	        int visited = -1;  
+	        for(int i = 0; i < b.length; i++){  
+	            int count = 1;  
+	            for(int j = i+1; j < b.length; j++){  
+	                if(b[i] == b[j]){  
+	                    count++;  
+	                     
+	                    c[j] = visited;  
+	                }  
+	            }  
+	            if(c[i] != visited)  
+	                c[i] = count;  
+	        }  
+	  
+	       
+	        
+	        for(int i = 0; i < c.length; i++){  
+	            if(c[i] != visited)  
+	                System.out.println(b[i] + "-" + c[i]);  
+	        }  
+	        
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Task t=new Task();
+		t.getFirstValue();
+		System.out.println("--------------------");
+		t.getLastValue();
+		System.out.println("--------------------");
+		t.getAverage();
+		System.out.println("--------------------");
+		t.Index();
+		System.out.println("--------------------");
+		t.count();
+	
+
+	}
+
+}
